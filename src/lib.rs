646b6391707e6cc2dfdad3d6fe63b5a546b71039
Moritz Moeller <virtualritz@gmail.com>
2020-07-09 23:57:28 +0200

    Suppress i128 warning.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 #![allow(clippy::all)]
+#![allow(improper_ctypes)]
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/ndspy-bindings.rs"));
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
