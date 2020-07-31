84e48dd9181126b2520ba30098b5fffa9f99c4db
Moritz Moeller <virtualritz@gmail.com>
2020-07-31 14:57:14 +0200

    Removed lib3delight dep. Cleanup. Bump to v0.1.3.

--- build.rs
+++ build.rs
@@ -1,87 +1,45 @@
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
 // build.rs
 extern crate bindgen;
 
-use std::env;
-use std::path::{Path, PathBuf};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let wrapper = "include/wrapper.h";
+    // TODO: make this generic & work on Linux/Windows
 
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed={}", wrapper);
+    println!("cargo:rerun-if-env-changed=DELIGHT");
 
-    let (include_path, lib_path) = match &env::var("DELIGHT") {
-        Err(_) => {
-            eprintln!("Building against 3Delight 2.1.2");
-
-            let lib_path = PathBuf::from(&env::var("OUT_DIR")?);
-
-            #[cfg(feature = "download_3delight_lib")]
-            {
-                use std::io::Write;
-
-                #[cfg(target_os = "windows")]
-                let lib = "https://www.dropbox.com/s/9iavkggor0ecc1x/3Delight.dll";
-                #[cfg(target_os = "macos")]
-                let lib = "https://www.dropbox.com/s/7vle92kcqbbyn8o/lib3delight.dylib";
-                #[cfg(target_os = "linux")]
-                let lib = "https://www.dropbox.com/s/hr62te8yg1d2e36/lib3delight.so";
-
-                let lib_file_path = lib_path.join(Path::new(lib).file_name().unwrap());
-
-                if !lib_file_path.exists() {
-                    // Download the libs to build against.
-                    // We do not care of this fails (yet)
-                    // as this is only needed when the
-                    // crate is linked against.
-                    (|| {
-                        let lib_data = reqwest::blocking::get(lib).ok()?.bytes().ok()?;
-                        std::fs::File::create(lib_file_path)
-                            .ok()?
-                            .write_all(&lib_data)
-                            .ok()?;
-                        Some(())
-                    })();
-                }
-            }
-
-            (
-                PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("include"),
-                lib_path,
-            )
-        }
+    let include_path = match &env::var("DELIGHT") {
+        Err(_) => PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("include"),
         Ok(path) => {
             eprintln!("Building against locally installed 3Delight @ {}", &path);
             let delight = Path::new(&path);
-
-            (delight.join("include"), delight.join("lib"))
+            delight.join("include")
         }
     };
 
     eprintln!("include: {}", include_path.display());
-    eprintln!("lib:     {}", lib_path.display());
-
-    // Emit linker searchpath
-    println!("cargo:rustc-link-search={}", lib_path.display());
-    // Link to lib3delight
-    println!("cargo:rustc-link-lib=dylib=3delight");
 
     // Build bindings
     let bindings = bindgen::Builder::default()
-        .header(wrapper)
+        .header("include/wrapper.h")
+        // Searchpath
+        .clang_arg(format!("-I{}", include_path.display()))
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Searchpath
-        .clang_arg(format!("-I{}", include_path.display()))
         .generate()
         .expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     bindings
-        .write_to_file(out_path.join("ndspy-bindings.rs"))
-        .expect("Couldn't write bindings.");
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Could not write bindings.");
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,4 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
-include!(concat!(env!("OUT_DIR"), "/ndspy-bindings.rs"));
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
