1d87576e156dd1c534700c49ddb924795b89b666
Moritz Moeller <virtualritz@gmail.com>
2020-07-31 18:30:34 +0200

    Improved docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! This is a thin, auto-generated wrapper around the
+//! [NSI](https://nsi.readthedocs.io/)/[RenderMan®](https://renderman.pixar.com/)
+//! low level display driver C API.
+//! # Documentation
+//! Pixar’s Developer’s Guide for [Display Driver
+//! Plug-Ins](https://renderman.pixar.com/resources/RenderMan_20/dspyNote.html)
+//! has in-depth information on this API that maps 1:1 to Rust.
+//! # Example
+//! For a Rust example demonstrting how to use this see the
+//! [r-display](https://github.com/virtualritz/r-display) crate.
+//!
 #![allow(clippy::all)]
 #![allow(improper_ctypes)]
 #![allow(non_camel_case_types)]
